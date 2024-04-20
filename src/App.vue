881ea66451a8e0268a229bc74bd418d041276c92
cuichengkang <14197780+cuichengkang@user.noreply.gitee.com>
2024-04-20 12:02:54 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Demo from './components/Demo.vue'
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <Demo/>
+  <HelloWorld/>
+</template>
+
+
---
+++ src/components/Demo.vue
@@ -0,0 +1,8 @@
+<template>
+    <!--编写HTML代码--> 
+    <h2>今天是周五</h2>
+</template>
+
+<script setup lang="ts">
+    //编写ts语句
+</script>
